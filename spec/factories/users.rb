0ce9b87550f4d1d07ae92363941594cad6067c4c
luis <hilario.huaman.6@gmail.com>
2022-07-16 16:42:20 -0500

    add test model user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { "MyString" }
-    password_digest { "MyString" }
+    email { Faker::Internet.email }
+    password_digest { Faker::Internet.password(min_length: 10, max_length: 20) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Valiaciones del modelo user' do
+    subject { build(:user) }
+    it 'validar si el email esta presente' do
+      should validate_presence_of(:email)
+    end
+    it 'validar si el password esta presente' do
+      should validate_presence_of(:password_digest)
+    end
+    it 'validar si el correo es unico' do
+      should validate_uniqueness_of(:email)
+    end
+    it 'validar que no acepte correos incorrectos' do
+      should_not allow_value('test').for(:email)
+    end
+  end
 end
