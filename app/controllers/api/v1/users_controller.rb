43f3ce44a731feadd9c64bc6d588535869ee52a3
luis <hilario.huaman.6@gmail.com>
2022-07-16 17:00:40 -0500

    creating and passing the user creation test

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,18 @@
 
 class Api::V1::UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @user.errors.messages }, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
---
+++ spec/controllers/users/create_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::UsersController, type: :controller do
+  describe 'Registro de usuarios' do
+    let(:user) { {  email: Faker::Internet.email,
+                    password: Faker::Internet.password(min_length: 10, max_length: 20)
+                  }
+                }
+    context 'Usuario registrado correctamente' do
+      before do # se ejecuta antes de iniciar las pruebas
+        post(:create, format: :json, params: { user: user })
+      end
+      context 'Respuesta con status created' do
+        subject { response } # el objeto a evaluar
+        it { is_expected.to have_http_status(:created) }
+      end
+      context 'Respuesta con valores correctos de user' do
+        subject { payload_test } # desde helpers/application_helper/method convirtiendo a json
+        it { is_expected.to include(:id, :email) }
+      end
+    end
+
+    let(:bad_user) { { email: 'test', password: '123456'}}
+    context 'Usuario incorrecto' do
+      before do
+        post(:create, format: :json, params: { user: bad_user })
+      end
+      context 'Respuesta con status bad request' do
+        subject { response }
+        it { is_expected.to have_http_status(:bad_request) }
+      end
+      context 'Respuesta con errores de validacion' do
+        subject { payload_test }
+        it { is_expected.to include(:errors) }
+      end
+    end
+  end
+end
